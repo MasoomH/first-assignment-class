7a8ca67e492fc8fffd4246657a789ace7455e642
MasoomH <hussainimasoom1@gmail.com>
2022-07-26 18:09:09 -0400

    second commit from local repository

---
+++ src/assignmentProject/SecondTestClass.java
@@ -0,0 +1,10 @@
+package assignmentProject;
+
+public class SecondTestClass {
+
+	public static void main(String[] args) {
+		System.out.println("Text from second class for GitHub test");
+
+	}
+
+}
